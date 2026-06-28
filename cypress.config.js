0150c300c7e0b38ddf349787804f1a5f6b5346bd
Bruno Shiroma <brunoshiroma@gmail.com>
2026-06-28 18:59:27 -0300

    fix: public production baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000/',
+    baseUrl: 'https://dev-toolbelt.brunoshiroma.com',
     viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 10000,
